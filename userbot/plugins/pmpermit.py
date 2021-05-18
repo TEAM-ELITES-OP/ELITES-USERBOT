1539ebf5bdcc6e8ff507542b97699aeeddd2ffa7
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-05-18 15:00:14 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -30,7 +30,7 @@ h1m4n5hu0p = (
     if CUSTOM_PMPERMIT
     else "**YOU HAVE TRESPASSED TO MY MASTERS INBOX** \n THIS IS ILLEGAL AND REGARDED AS CRIME"
 )
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Mafia User"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "savage User"
 USER_BOT_WARN_ZERO = "**You were spamming my sweet master's inbox, henceforth you have been blocked by my master's savage bot.**\n__Now GTFO, i'm busy__"
 USER_BOT_NO_WARN = (
     "Hello, This is **𝐒𝐀𝐕𝐀𝐆𝐄 Ultra Private Security Protocol⚠️**.\n"
