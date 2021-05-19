b71268df565ff0910ed56b826871d119fa721b7a
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-05-19 09:42:45 +0530

    Update zipfile.py

--- userbot/plugins/zipfile.py
+++ userbot/plugins/zipfile.py
@@ -6,79 +6,44 @@ import zipfile
 from mafiabot.utils import admin_cmd, sudo_cmd, edit_or_reply
 
 
-@bot.on(admin_cmd(pattern="compress ?(.*)", outgoing=True))
-@bot.on(sudo_cmd(pattern="compress ?(.*)", allow_sudo=True))
+
+@bot.on(admin_cmd("zip"))
 async def _(event):
-
     if event.fwd_from:
-
         return
-
-    input_str = event.pattern_match.group(1)
-
-    mone = await edit_or_reply(event, "Processing ...")
-
+    if not event.is_reply:
+        await event.edit("Reply to a file to compress it. Bruh.")
+        return
+    mone = await event.edit("Processing ...")
     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-
     if event.reply_to_msg_id:
-
         reply_message = await event.get_reply_message()
-
         try:
-
-            time.time()
-
             downloaded_file_name = await borg.download_media(
-                reply_message, Config.TMP_DOWNLOAD_DIRECTORY
+                reply_message,
+                Config.TMP_DOWNLOAD_DIRECTORY,
             )
-
             directory_name = downloaded_file_name
-
-            await edit_or_reply(event, "Finish downloading to my local")
-
-            zipfile.ZipFile(directory_name + ".zip", "w", zipfile.ZIP_DEFLATED).write(
-                directory_name
-            )
-
-            await borg.send_file(
-                event.chat_id,
-                directory_name + ".zip",
-                caption="Zipped By [MafiaBot](t.me/MafiaBot_Support)",
-                force_document=True,
-                allow_cache=False,
-                reply_to=event.message.id,
-            )
-
-            try:
-
-                os.remove(directory_name + ".zip")
-
-                os.remove(directory_name)
-
-            except:
-
-                pass
-
-            await edit_or_reply(event, "task Completed")
-
-            await asyncio.sleep(3)
-
-            await event.delete()
-
+            await event.edit(downloaded_file_name)
         except Exception as e:  # pylint:disable=C0103,W0703
-
             await mone.edit(str(e))
+    zipfile.ZipFile(directory_name + '.zip', 'w', zipfile.ZIP_DEFLATED).write(directory_name)
+    await borg.send_file(
+        event.chat_id,
+        directory_name + ".zip",
+        caption="**Zipped!**",
+        force_document=True,
+        allow_cache=False,
+        reply_to=event.message.id,
+    )
+    await asyncio.sleep(7)
+    await event.delete()
 
-    elif input_str:
 
-        directory_name = input_str
-
-        zipfile.ZipFile(directory_name + ".zip", "w", zipfile.ZIP_DEFLATED).write(
-            directory_name
-        )
-
-        await edit_or_reply(event, 
-            "Local file compressed to `{}`".format(directory_name + ".zip")
-        )
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file))
+            os.remove(os.path.join(root, file))
