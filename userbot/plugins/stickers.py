037f58247f0b68f7d9d572b48b7e8ac1a8347521
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-05-30 10:38:40 +0530

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -99,11 +99,11 @@ async def kang(args):
                 # pack
                 emoji = splat[1]
 
-        packname = f"HB_{user.username}_MafiaBot_{pack}"
+        packname = f"HB_{user.username}_SavageBot_{pack}"
         packnick = (
             f"{mafiabot} Vol.{pack}"
             if mafiabot
-            else f"@{user.username}'s MafiaBot Vol.{pack}"
+            else f"@{user.username}'s_Savage_Bot Vol.{pack}"
         )
         cmd = "/newpack"
         file = io.BytesIO()
@@ -139,7 +139,7 @@ async def kang(args):
                     packnick = (
                         f"{mafiabot} Vol.{pack}"
                         if mafiabot
-                        else f"@{user.username}'s MafiaBot Vol.{pack}"
+                        else f"@{user.username}'s Savage_Bot Vol.{pack}"
                     )
                     await args.edit(
                         "`Switching to Pack "
