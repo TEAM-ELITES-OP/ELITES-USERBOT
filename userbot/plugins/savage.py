5c4c161fea6cbc5188ab6c0875aaadbc9b20ff80
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-05-31 16:05:22 +0530

    Update savage.py

--- userbot/plugins/savage.py
+++ userbot/plugins/savage.py
@@ -6,13 +6,13 @@ from userbot import xbot, BOT
 import os,re
 from telethon.tl.custom import Button 
 from telethon import events, errors, custom, functions
-@xbot.on(events.InlineQuery(pattern=r"repo"))
+@tgbot.on(callbackquery.CallbackQuery(data=re.compile(b"savage")))
 async def inline_id_handler(event: events.InlineQuery.Event):
  mafiabot = event.builder
  X= [[custom.Button.inline("🔥 Cʟɪᴄᴋ Hᴇʀᴇ 🔥",data="oobhai")]]
  query = event.text
  result = mafiabot.article("ᴅᴀɪsʏX",text="**ᴅᴀɪsʏX's Rᴇᴘᴏ, Dᴇᴘʟᴏʏ ᴀɴᴅ Sᴜᴘᴘᴏʀᴛ\n\n© @ULTRAXOT**",buttons=X,link_preview=False)
  await event.answer([result])
-@xbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"obhai")))
+@tgbot.on(callbackquery.CallbackQuery(data=re.compile(b"oobhai")))
 async def callback_query_handler(event):
   await event.edit(text=f"**{BOT} Rᴇᴘᴏ, Dᴇᴘʟᴏʏ ᴀɴᴅ Gʀᴏᴜᴘ Lɪɴᴋ\n\n© @ULTRAXOT**",buttons=[Button.url(f"⚜️ Rᴇᴘᴏ ⚜️", url="https://github.com/ULTRA-OP/ULTRA-X")])
