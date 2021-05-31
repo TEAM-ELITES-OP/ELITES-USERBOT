68d0cea359475b0212243097ea889166b6261e5c
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-05-31 08:38:35 +0530

    Update history.py

--- userbot/plugins/history.py
+++ userbot/plugins/history.py
@@ -1,25 +1,25 @@
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from SAVAGE import bot, CmdHelp
-from SAVAGE.utils import admin_cmd, edit_or_reply as eor, sudo_cmd
+from mafiabot import bot, CmdHelp
+from mafiabot.utils import admin_cmd, edit_or_reply as eor, sudo_cmd
 
-@SAVAGE.on(admin_cmd(pattern="history ?(.*)"))
-@SAVAGE.on(sudo_cmd(pattern="history ?(.*)", allow_sudo=True))
+@mafiabot.on(admin_cmd(pattern="history ?(.*)"))
+@mafiabot.on(sudo_cmd(pattern="history ?(.*)", allow_sudo=True))
 async def _(mafiaevent):
-    if SAVAGEevent.fwd_from:
+    if mafiaevent.fwd_from:
         return 
-    if not SAVAGEevent.reply_to_msg_id:
-       await eor(SAVAGEevent, "`Please Reply To A User To Get This Module Work`")
+    if not mafiaevent.reply_to_msg_id:
+       await eor(mafiaevent, "`Please Reply To A User To Get This Module Work`")
        return
-    reply_message = await SAVAGEevent.get_reply_message() 
+    reply_message = await mafiaevent.get_reply_message() 
     chat = "Sangmatainfo_bot"
     victim = reply_message.sender.id
     if reply_message.sender.bot:
-       await eor(SAVAGEevent, "Need actual users. Not Bots")
+       await eor(mafiaevent, "Need actual users. Not Bots")
        return
-    await eor(SAVAGEevent, "Checking...")
-    async with SAVAGEevent.client.conversation(chat) as conv:
+    await eor(mafiaevent, "Checking...")
+    async with mafiaevent.client.conversation(chat) as conv:
           try:     
               response1 = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
               response2 = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
@@ -29,13 +29,13 @@ async def _(mafiaevent):
               response2 = await response2 
               response3 = await response3 
           except YouBlockedUserError: 
-              await SAVAGEevent.reply("Please unblock ( @Sangmatainfo_bot ) ")
+              await mafiaevent.reply("Please unblock ( @Sangmatainfo_bot ) ")
               return
           if response1.text.startswith("No records found"):
-             await eor(SAVAGEevent, "User never changed his Username...")
+             await eor(mafiaevent, "User never changed his Username...")
           else: 
-             await SAVAGEevent.delete()
-             await SAVAGEevent.client.send_message(mafiaevent.chat_id, response2.message)
+             await mafiaevent.delete()
+             await mafiaevent.client.send_message(mafiaevent.chat_id, response2.message)
 
 @mafiabot.on(admin_cmd(pattern="unh ?(.*)"))
 @mafiabot.on(sudo_cmd(pattern="unh ?(.*)", allow_sudo=True))
