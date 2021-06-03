23cb23977a18a7c0437fd058d788e57acd5950db
TEAM-ELITES-OP <85295313+TEAM-ELITES-OP@users.noreply.github.com>
2021-06-03 19:51:25 +0530

    Update and rename _mafiabot.py to _elitesbot.py

--- userbot/plugins/_elitesbot.py
+++ userbot/plugins/_elitesbot.py
@@ -1,13 +1,13 @@
-"""SAVAGEBot Help Command"""
+"""elitesbot Help Command"""
 
 from userbot import *
 from userbot import CMD_HELP
-from SAVAGEbot.utils import *
+from elitesbot.utils import *
 
 
 @bot.on(admin_cmd(pattern="plinfo(?: |$)(.*)", outgoing=True))
 @bot.on(sudo_cmd(pattern="plinfo(?: |$)(.*)", allow_sudo=True))
-async def SAVAGEbott(event):
+async def elitesbott(event):
     if event.fwd_from:
         return
     """ .plinfo cmd """
