845b383a5b60e748cf189d276a72fb1bb909d9a4
TEAM-ELITES-OP <85295313+TEAM-ELITES-OP@users.noreply.github.com>
2021-06-04 11:35:11 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -126,7 +126,7 @@ def admin_cmd(pattern=None, command=None, **args):
             elif len(Config.COMMAND_HAND_LER) == 1:
                 elitesbot = "^\\" + Config.COMMAND_HAND_LER
                 reg = Config.COMMAND_HAND_LER
-            args["pattern"] = re.compile(regelitesbot + pattern)
+            args["pattern"] = re.compile(elitesbot + pattern)
             if command is not None:
                 cmd = reg + command
             else:
