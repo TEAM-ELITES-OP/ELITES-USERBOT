271de9d159b5cf3bb72d32b1a2f21196590d2bf4
TEAM-ELITES-OP <85295313+TEAM-ELITES-OP@users.noreply.github.com>
2021-06-04 17:09:44 +0530

    Update zipfile.py

--- userbot/plugins/zipfile.py
+++ userbot/plugins/zipfile.py
@@ -45,7 +45,7 @@ async def _(event):
             await borg.send_file(
                 event.chat_id,
                 directory_name + ".zip",
-                caption="Zipped By [SAVAGE BOT](t.me/SAVAGE_TEAM_BOLTE)",
+                caption="Zipped By [ELITES BOT](t.me/SAVAGE_TEAM_BOLTE)",
                 force_document=True,
                 allow_cache=False,
                 reply_to=event.message.id,
