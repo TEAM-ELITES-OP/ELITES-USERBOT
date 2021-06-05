992d53326dcf56480d7d95ea35f82a61d398dd32
Dipesh <83916289+xdipesh@users.noreply.github.com>
2021-06-05 23:04:03 +0530

    Update auto_profile.py

--- userbot/plugins/auto_profile.py
+++ userbot/plugins/auto_profile.py
@@ -9,9 +9,9 @@ from userbot import ALIVE_NAME, BIO_MSG, CMD_HELP
 from elitesbot.utils import admin_cmd, edit_or_reply, sudo_cmd
 from userbot.cmdhelp import CmdHelp
 
-DEFAULTUSERBIO = str(BIO_MSG) if BIO_MSG else "LEGENDARY AF MAFIABOT"
+DEFAULTUSERBIO = str(BIO_MSG) if BIO_MSG else "LEGENDARY AF ELITEBOT"
 DEL_TIME_OUT = 60
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Mafia User"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "elites  User"
 
 
 @bot.on(admin_cmd(pattern="autoname"))  # pylint:disable=E0602
