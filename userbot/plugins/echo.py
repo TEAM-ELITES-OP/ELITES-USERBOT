d9483547f031cd2a381a2f713caf80bb43a78ef0
Dipesh <83916289+xdipesh@users.noreply.github.com>
2021-06-06 14:46:38 +0530

    Update echo.py

--- userbot/plugins/echo.py
+++ userbot/plugins/echo.py
@@ -22,56 +22,56 @@ from userbot.cmdhelp import CmdHelp
 
 @bot.on(admin_cmd(pattern="echo$"))
 @bot.on(sudo_cmd(pattern="echo$", allow_sudo=True))
-async def echo(SAVAGE):
-    if SAVAGE.fwd_from:
+async def echo(elitebot):
+    if elitesbot.fwd_from:
         return
-    if SAVAGE.reply_to_msg_id is not None:
-        reply_msg = await SAVAGE.get_reply_message()
+    if elitebot.reply_to_msg_id is not None:
+        reply_msg = await elitebot.get_reply_message()
         user_id = reply_msg.sender_id
-        chat_id = SAVAGE.chat_id
+        chat_id = elitebot.chat_id
         try:
             eliteboy = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
             eliteboy = Get(eliteboy)
-            await SAVAGE.client(eliteboy)
+            await elitebot.client(eliteboy)
         except BaseException:
             pass
         if is_echo(user_id, chat_id):
-            await edit_or_reply(SAVAGE, "The user is already enabled with echo ")
+            await edit_or_reply(elitebot, "The user is already enabled with echo ")
             return
         addecho(user_id, chat_id)
-        await edit_or_reply(SAVAGE, "Hii....😄🤓")
+        await edit_or_reply(elitebot, "Hii....😄🤓")
     else:
-        await edit_or_reply(SAVAGE, "Reply to a User's message to echo his messages")
+        await edit_or_reply(elitebot, "Reply to a User's message to echo his messages")
 
 
 @bot.on(admin_cmd(pattern="rmecho$"))
 @bot.on(sudo_cmd(pattern="rmecho$", allow_sudo=True))
-async def echo(SAVAGE):
-    if SAVAGE.fwd_from:
+async def echo(elitebot):
+    if elitebot.fwd_from:
         return
-    if SAVAGE.reply_to_msg_id is not None:
-        reply_msg = await SAVAGE.get_reply_message()
+    if elitebot.reply_to_msg_id is not None:
+        reply_msg = await elitebot.get_reply_message()
         user_id = reply_msg.sender_id
-        chat_id = SAVAGE.chat_id
+        chat_id = elitebot.chat_id
         try:
             eliteboy = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
             eliteboy = Get(eliteboy)
-            await SAVAGE.client(eliteboy)
+            await elitebot.client(eliteboy)
         except BaseException:
             pass
         if is_echo(user_id, chat_id):
             remove_echo(user_id, chat_id)
-            await edit_or_reply(SAVAGE, "Echo has been stopped for the user")
+            await edit_or_reply(elitebot, "Echo has been stopped for the user")
         else:
-            await edit_or_reply(SAVAGE, "The user is not activated with echo")
+            await edit_or_reply(elitebot, "The user is not activated with echo")
     else:
-        await edit_or_reply(SAVAGE, "Reply to a User's message to echo his messages")
+        await edit_or_reply(elitebot, "Reply to a User's message to echo his messages")
 
 
 @bot.on(admin_cmd(pattern="listecho$"))
 @bot.on(sudo_cmd(pattern="listecho$", allow_sudo=True))
-async def echo(SAVAGE):
-    if SAVAGE.fwd_from:
+async def echo(elitebot):
+    if elitebot.fwd_from:
         return
     lsts = get_all_echos()
     if len(lsts) > 0:
@@ -93,25 +93,25 @@ async def echo(SAVAGE):
         )
         url = f"https://nekobin.com/{key}"
         reply_text = f"echo enabled users: [here]({url})"
-        await edit_or_reply(SAVAGE, reply_text)
+        await edit_or_reply(elitebot, reply_text)
     else:
-        await edit_or_reply(SAVAGE, output_str)
+        await edit_or_reply(elitebot, output_str)
 
 
 @bot.on(events.NewMessage(incoming=True))
-async def samereply(SAVAGE):
-    if SAVAGE.chat_id in Config.UB_BLACK_LIST_CHAT:
+async def samereply(elitebot):
+    if elitebot.chat_id in Config.UB_BLACK_LIST_CHAT:
         return
-    if is_echo(SAVAGE.sender_id, SAVAGE.chat_id):
+    if is_echo(elitebot.sender_id, elitebot.chat_id):
         await asyncio.sleep(2)
         try:
             eliteboy = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
             eliteboy = Get(eliteboy)
-            await SAVAGE.client(eliteboy)
+            await elitesbot.client(eliteboy)
         except BaseException:
             pass
-        if SAVAGE.message.text or SAVAGE.message.sticker:
-            await SAVAGE.reply(SAVAGE.message)
+        if elitesbot.message.text or elitesbot.message.sticker:
+            await elitesbot.reply(elitesbot.message)
 
 
 CmdHelp("echo").add_command(
